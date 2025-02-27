151eba0c0e3cbee1778702860af172678ea86fdb
NeoDev <neoforevershog@gmail.com>
2025-02-27 10:28:14 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 import os
 import platform
 import pkg_resources
+import setuptools
 from setuptools import find_packages, setup
+from pkg_resources import parse_version
 
-setup(
-    name="infer_rvc_python",
-    version="1.2.0",
-    description="Python wrapper for fast inference with rvc",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    readme="README.md",
-    python_requires=">=3.10",
-    author="R3gm",
-    url="https://github.com/R3gm/infer_rvc_python",
-    license="MIT",
-    packages=find_packages(),
-    package_data={'': ['*.txt', '*.rep', '*.pickle']},
-    install_requires=[
+# Read the long description with explicit encoding
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup_kwargs = {
+    "name": "infer_rvc_python",
+    "version": "1.2.0",
+    "description": "Python wrapper for fast inference with rvc",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "python_requires": ">=3.10",
+    "author": "R3gm",
+    "url": "https://github.com/R3gm/infer_rvc_python",
+    "license": "MIT",
+    "packages": find_packages(),
+    "package_data": {"": ["*.txt", "*.rep", "*.pickle"]},
+    "install_requires": [
         "torch",
         "torchaudio",
         "gradio",
@@ -28,15 +33,24 @@ setup(
         "torchcrepe==0.0.20",
         "ffmpeg-python>=0.2.0",
         "fairseq==0.12.2",
+        "transformers",
         "typeguard==4.2.0",
         "soundfile",
         "librosa",
         "numpy",
     ],
-    include_package_data=True,
-    extras_require={"all": [
-        "scipy",
-        "numba==0.56.4",
-        "edge-tts"
-        ]},
-)
+    "include_package_data": True,
+    "extras_require": {
+        "all": [
+            "scipy",
+            "numba==0.56.4",
+            "edge-tts"
+        ]
+    },
+}
+
+# Conditionally include the "readme" parameter if supported
+if parse_version(setuptools.__version__) >= parse_version("68.0.0"):
+    setup_kwargs["readme"] = "README.md"
+
+setup(**setup_kwargs)
